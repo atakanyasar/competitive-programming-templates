8ab6dc0540a5a302ad3c94ff8007ed975cd14932
theStaticMind <62073188+theStaticMind@users.noreply.github.com>
2020-03-14 21:01:55 +0300

    Update convex_hull.cpp

--- convex_hull.cpp
+++ convex_hull.cpp
@@ -1,3 +1,86 @@
+bool Q = true;
+
+struct Line{
+
+	mutable int m, n;
+	mutable long double p;
+
+	Line(){}
+	Line(int _m, int _n, long double _p = INF): m(_m), n(_n), p(_p){}
+
+	bool operator<(const Line& X)const{
+
+		if(Q) return m < X.m; // max
+
+		else return p < X.p;
+
+	}
+
+};
+
+struct Hull : multiset<Line> {
+
+	long double cross(iterator A, iterator B){
+
+		long double m1 = A->m, m2 = B->m;
+		long double n1 = A->n, n2 = B->n;
+
+		if(m1 == m2) return n1 >= n2 ? INF : -INF; // max
+
+		return (n1 - n2) / (m2 - m1); 
+
+	}
+
+	bool isect(iterator A, iterator B){
+
+		if(B == end()){
+
+			A->p = INF;
+
+			return false;
+
+		}
+
+		A->p = cross(A, B);
+
+		return !(A->p < B->p);
+
+	}
+
+	void add(int m, int n){
+
+		iterator curr = insert(Line(m, n));
+
+		while(isect(curr, next(curr))) erase(next(curr));
+
+		if(curr == begin()) return;
+
+		while(isect(prev(curr), curr)) curr = erase(curr);
+
+		while(prev(curr) != begin() && isect(prev(prev(curr)), prev(curr))){
+
+			erase(prev(curr));
+
+			isect(prev(curr), curr);
+
+		}
+
+	}
+
+	int query(int x){
+
+		Q = false;
+		iterator curr = lower_bound(Line(0, 0, x));
+		Q = true;
+
+		return curr->m * x + curr->n;
+
+	}
+
+};
+
+
+
 bool Q=false;
 struct Line{
     mutable int m,c;
