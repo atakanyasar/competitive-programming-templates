006008090ef5077442fef92f840f262f45dbcf8e
theStaticMind <62073188+theStaticMind@users.noreply.github.com>
2020-03-16 15:46:41 +0300

    Update Treap.cpp

--- Treap.cpp
+++ Treap.cpp
@@ -1,157 +1,149 @@
-#include<bits/stdc++.h>
-#define pb push_back
-#define ii pair<int,int>
-#define all(x) (x).begin(),(x).end()
-#define sz(x) ((int)(x).size())
-#define INF 100000000000000000
-#define modulo 1000000007
-#define mod 998244353
-#define int long long int
-using namespace std;
-
 #define rand() (rand() * rand() + rand() + 1)
-
+ 
 struct Node{
-
-      int left, right, value, prior;
-
-      void set(int a, int b, int c, int d){
-            
-            left = a;
-            right = b;
-            value = c;
-            prior = d;
-
-      }
-      
-      Node(){
-            left = right = value = 0;
-            prior = 0;
-      }
-
-} node[3000000];
-
-
+ 
+	int left, right;
+	int key;
+	int prior;
+ 
+	Node(){
+		left = right = key = prior = 0;
+	}
+ 
+};
+ 
+Node node[2000000];
+ 
 namespace Treap{
-
-	int ind = 1;
+ 
 	int root = 0;
-
+	int ind = 1;
+ 
 	int new_node(){
 		return ind++;
 	}
-
-	void split(int curr, int x, int& l, int& r){
-
-		if(!curr) l = r = 0;
-
-		else if(x < node[curr].value){
-			split(node[curr].left, x, l, node[curr].left);
-			r = curr;
+ 
+	void push(int j){
+ 
+		if(!j) return;
+ 
+		int l = node[j].left;
+		int r = node[j].right;
+  
+		if(l){
+			
 		}
-		else{
-			split(node[curr].right, x, node[curr].right, r);
-			l = curr;
+ 
+		if(r){
+			
 		}
-
+ 
+ 
 	}
-
-	void merge(int& curr, int l, int r){
-
-		if(!l || !r){
-			curr = max(l, r);
-		}
-		else{
-			if(node[l].prior > node[r].prior){
-				merge(node[l].right, node[l].right, r);
-				curr = l;
-			}
-			else{
-				merge(node[r].left, l, node[r].left);
-				curr = r;
-			}
-		}
-
+ 
+	void up(int j){
+ 
+		if(!j) return;
+ 
+		int l = node[j].left;
+		int r = node[j].right;
+ 
+ 
 	}
-
-	void insert(int& curr, int x, int p){
+ 
+	void split(int j, int v, int& l, int& r){
+ 
+		push(j);
+ 
+		if(!j) l = r = 0;
+ 
+		else if(node[j].key < v){
+ 
+			split(node[j].right, v, node[j].right, r);
+ 
+			l = j;
+ 
+		}
+ 
+		else{
+ 
+			split(node[j].left, v, l, node[j].left);
+ 
+			r = j;
+ 
+		}
 		
-		if(!curr){
-			curr = new_node();
-			node[curr].set(0, 0, x, p);
-		}
+		push(j);
+		push(node[j].left);
+		push(node[j].right);
+		up(j);
+ 
+	}
+ 
+	void merge(int& j, int l, int r){
+ 
+		push(l);
+		push(r);
+ 
+		if(!l || !r) j = max(l, r);
+ 
+		else if(node[l].prior > node[r].prior){
+ 
+			merge(node[l].right, node[l].right, r);
+ 
+			j = l;
+ 
+		} 
+ 
 		else{
-			if(p >= node[curr].prior){
-				int temp = curr;
-				curr = new_node();
-				node[curr].set(0, 0, x, p);
-
-				split(temp, x, node[curr].left, node[curr].right);
-			}
-			else{
-				if(x < node[curr].value) insert(node[curr].left, x, p);
-				else insert(node[curr].right, x, p);
-			}
+ 
+			merge(node[r].left, l, node[r].left);
+ 
+			j = r;
 		}
-
+ 
+		push(j);
+		push(node[j].left);
+		push(node[j].right);
+		up(j);
+ 
 	}
-
-	int& search(int& curr, int x){
-		if(!curr) return curr;
-		if(x == node[curr].value) return curr;
-		else if(x < node[curr].value) return search(node[curr].left, x);
-		else return search(node[curr].right, x);
+ 
+	void insert(int& j, int i){
+ 
+		push(j);
+ 
+		if(!j) j = i;
+ 
+		else if(node[i].prior > node[j].prior){
+ 
+			split(j, node[i].key, node[i].left, node[i].right);
+ 
+			j = i;
+ 
+		}
+		else {
+			if(node[i].key < node[j].key) insert(node[j].left, i);
+			else insert(node[j].right, i);
+		}
+ 
+		push(node[j].left);
+		push(node[j].right);
+		up(j);
+ 
 	}
-
-	void erase(int x){
-		int& curr = search(root, x);
-		merge(curr, node[curr].left, node[curr].right);
+ 
+	int add(int v){
+		int j = new_node();
+		node[j].key = v;
+		node[j].prior = rand();
+		insert(root, j);
+		return j;
 	}
-
-	ii debug(int x){
-            if(!x) return {0, 0};
-            int l = node[x].left;
-            int r = node[x].right;
-            ii P = {node[x].value, node[x].value};
-            if(l) {
-       //     	cout << node[x].value << " " << node[l].value <<"\n";
-            	P.second = max(P.second, debug(node[x].left).second);
-            	assert(node[x].prior >= node[l].prior);
-            	assert(node[x].value == P.second);
-            }
-            if(r) {
-        //    	cout << node[x].value << " " << node[r].value << "\n";
-                  P.first = min(P.first, debug(node[x].right).first);
-                  assert(node[x].prior >= node[r].prior);
-            	assert(node[x].value == P.first);
-            }
-            return P;
-      }
-
-}
-
-int32_t main(){
-      ios_base::sync_with_stdio(false);
-      cin.tie(NULL);
-      freopen("q.gir", "r", stdin);
-      freopen("q.cik", "w", stdout);
-      srand(time(NULL));
-      vector<int> arr;
-      for(int i = 1; i <= 5000; i++)arr.pb(i);
-      for(int i = 0; i < 5000; i++){
-      	int w = rand() % sz(arr);
-      	Treap::insert(Treap::root, arr[w], rand());
-      	arr.erase(arr.begin() + w);
-      }
-      for(int i = 1; i <= 5000; i++){
-      	Treap::erase(i);
-      	Treap::debug(Treap::root);
-      }
-      
- /*     vector<ii> A;
-      for(int i = 1; i < Treap::ind; i++){
-      	A.emplace_back(node[i].prior, node[i].value);
-      }
-      sort(all(A),greater<ii>());
-      for(int i = 0; i < A.size(); i++) cout << A[i].second << "\n";*/
+ 
+ 
+	void update(){
+ 
+ 
+	}
 }
