cd8d415b9dc7bcc7a89108aa98d42eb03731181e
theStaticMind <62073188+theStaticMind@users.noreply.github.com>
2020-03-19 20:47:54 +0300

    Update hash.cpp

--- hash.cpp
+++ hash.cpp
@@ -1,16 +1,10 @@
 namespace HASH{
 
-	unsigned int exp(unsigned int a, int b){
-		if(b == 0)return 1;
-		if(b % 2)return a * exp(a, b - 1);
-		int t = exp(a, b / 2);
-		return t * t;
+	void getexp(vector<int>& exp, int x, int m){
+		for(int i = 1; i < sz(exp); i++) exp[i] = exp[i - 1] * x % m;
 	}
-	int exp(int a, int b, int m){
-		if(b == 0)return 1;
-		if(b % 2)return a * exp(a, b - 1, m) % m;
-		int t = exp(a, b / 2, m);
-		return t * t % m;
+	void getexp(vector<unsigned int>& exp, unsigned int x){
+		for(int i = 1; i < sz(exp); i++) exp[i] = exp[i - 1] * x;
 	}
 
 	void prefHash(vector<int>& _hash, string& s, int x, int m){
@@ -34,17 +28,17 @@ namespace HASH{
 		}
 	}
 
-	int getpHash(vector<int>& _hash, int x, int m, int l, int r){
-		return (_hash[r] - exp(x, r - l + 1, m) * _hash[l - 1] % m + m) % m;
+	int getpHash(vector<int>& _hash, vector<int>& exp, int x, int m, int l, int r){
+		return (_hash[r] - exp[r - l + 1] * _hash[l - 1] % m + m) % m;
 	}
-	unsigned int getpHash(vector<unsigned int>& _hash, unsigned int x, int l, int r){
-		return (_hash[r] - exp(x, r - l + 1) * _hash[l - 1]);
+	unsigned int getpHash(vector<unsigned int>& _hash, vector<unsigned int>& exp, unsigned int x, int l, int r){
+		return (_hash[r] - exp[r - l + 1] * _hash[l - 1]);
 	}
 
-	int getsHash(vector<int>& _hash, int x, int m, int l, int r){
-		return (_hash[l] - exp(x, r - l + 1, m) * _hash[r + 1] % m + m) % m;
+	int getsHash(vector<int>& _hash, vector<int>& exp, int x, int m, int l, int r){
+		return (_hash[l] - exp[r - l + 1] * _hash[r + 1] % m + m) % m;
 	}
-	unsigned int getsHash(vector<unsigned int>& _hash, unsigned int x, int l, int r){
-		return (_hash[l] - exp(x, r - l + 1) * _hash[r + 1]);
+	unsigned int getsHash(vector<unsigned int>& _hash, vector<unsigned int>& exp, unsigned int x, int l, int r){
+		return (_hash[l] - exp[r - l + 1] * _hash[r + 1]);
 	}
 }
