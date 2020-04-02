579d8e291606eaea54f993a9f83a2e850896439b
theStaticMind <62073188+theStaticMind@users.noreply.github.com>
2020-04-02 20:45:18 +0300

    Update Treap.cpp

--- Treap.cpp
+++ Treap.cpp
@@ -5,9 +5,17 @@ struct Node{
 	int left, right;
 	int key;
 	int prior;
- 
-	Node(){
+
+	Node(int k = 0){
 		left = right = key = prior = 0;
+		key = k;
+	}
+
+	bool operator<(Node& A){
+		return key < A.key;
+	}
+	bool operator==(Node& A){
+		return key == A.key;
 	}
  
 };
@@ -22,31 +30,30 @@ namespace Treap{
 	int new_node(){
 		return ind++;
 	}
-	
-	
- 
+
+
+
 	void push(int j){
- 
+
 		if(!j) return;
- 
+
 		int l = node[j].left;
 		int r = node[j].right;
-  
+
 		if(l){
 			
 		}
- 
+
 		if(r){
 			
 		}
- 
- 
+
+
 	}
-	
+
 	void print(int j){
 		if(!j)return;
 		push(j);
-		cout << node[j].key << " " << node[j].val.second << "\n"; 
 
 		int l = node[j].left;
 		int r = node[j].right;
@@ -58,33 +65,33 @@ namespace Treap{
 			print(r);
 		}
 	}
- 
+
 	void up(int j){
- 
+
 		if(!j) return;
- 
+
 		int l = node[j].left;
 		int r = node[j].right;
- 
- 
+
+
 	}
- 
-	void split(int j, int v, int& l, int& r){
- 
+
+	void split(int j, Node v, int& l, int& r){
+
 		push(j);
- 
+
 		if(!j) l = r = 0;
- 
-		else if(node[j].key < v){
- 
+
+		else if(node[j] < v){
+
 			split(node[j].right, v, node[j].right, r);
- 
+
 			l = j;
- 
+
 		}
- 
+
 		else{
- 
+
 			split(node[j].left, v, l, node[j].left);
  
 			r = j;
@@ -97,51 +104,51 @@ namespace Treap{
 		up(j);
  
 	}
- 
+
 	void merge(int& j, int l, int r){
- 
+
 		push(l);
 		push(r);
- 
+
 		if(!l || !r) j = max(l, r);
- 
+
 		else if(node[l].prior > node[r].prior){
- 
+
 			merge(node[l].right, node[l].right, r);
- 
+
 			j = l;
- 
+
 		} 
- 
+
 		else{
- 
+
 			merge(node[r].left, l, node[r].left);
- 
+
 			j = r;
 		}
- 
+
 		push(j);
 		push(node[j].left);
 		push(node[j].right);
 		up(j);
- 
+
 	}
- 
+
 	void insert(int& j, int i){
- 
+
 		push(j);
- 
+
 		if(!j) j = i;
- 
+
 		else if(node[i].prior > node[j].prior){
- 
+
 			split(j, node[i].key, node[i].left, node[i].right);
- 
+
 			j = i;
- 
+
 		}
 		else {
-			if(node[i].key < node[j].key) insert(node[j].left, i);
+			if(node[i] < node[j]) insert(node[j].left, i);
 			else insert(node[j].right, i);
 		}
  
@@ -151,17 +158,17 @@ namespace Treap{
  
 	}
 	
-	int erase(int& j, double v){
+	int erase(int& j, Node v){
 
 		push(j);
 
-		if(node[j].key == v) {
+		if(node[j] == v) {
 			int ret = j;
 			merge(j, node[j].left, node[j].right);
 			return ret;
 		}
 		else{
-			if(v < node[j].key) return erase(node[j].left, v);
+			if(v < node[j]) return erase(node[j].left, v);
 			else return erase(node[j].right, v);
 		}
 		
