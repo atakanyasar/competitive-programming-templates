43d604c314876641a3ba67df8b2f2c1d2e2258ab
Atakan Yaşar <atakany357@gmail.com>
2023-06-29 18:13:11 +0300

    added double hashing

--- hashing.h
+++ hashing.h
@@ -1,5 +1,9 @@
-struct Hash{
-	vector<int64_t> prefix_hash, suffix_hash;
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Hash {
+
+    vector<int64_t> prefix_hash, suffix_hash;
 	vector<int64_t> exp;
 	string s;
 	int64_t X;
@@ -28,4 +32,21 @@ struct Hash{
 	int64_t suffixHash(int64_t l, int64_t r){
 		return (suffix_hash[l] - exp[r - l + 1] * suffix_hash[r + 1] % M + M) % M;
 	}
+
+};
+
+struct DoubleHash {
+
+    Hash h1, h2;
+    DoubleHash(string s, int64_t x1, int64_t x2, uint64_t m = ULONG_LONG_MAX)
+    : h1(s, x1, m), h2(s, x2, m)
+    {}
+
+    pair<int64_t, int64_t> prefixHash(int64_t l, int64_t r){
+        return {h1.prefixHash(l, r), h2.prefixHash(l, r)};
+    }
+    pair<int64_t, int64_t> suffixHash(int64_t l, int64_t r){
+        return {h1.suffixHash(l, r), h2.suffixHash(l, r)};
+    }
+
 };
